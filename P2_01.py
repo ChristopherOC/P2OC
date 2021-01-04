acd50c6c640d87bf43b8f82def228a80afd93cac
Rene Christopher <crene.contact@gmail.com>
2021-01-04 13:45:33 +0100

    apres seance du 29/12/20

--- P2_01.py
+++ P2_01.py
@@ -1,33 +1,34 @@
+import pip
 import requests
 from bs4 import BeautifulSoup
+import argparse
 
-def bookOne():
-    url = input("Saisissez l'url du livre à scrapper : ")  # URL à scraper
-    print("L'url :", url)  # Affiche l'Url produit
-    r = requests.get(url)  # Cherche l'Url en question
-    if r.ok:  # Si l'url existe
-        r.encoding = 'utf8'
-        soup = BeautifulSoup(r.text, 'lxml')
-        titre = soup.find('h1')  # Trouve le titre
-        print("Le titre est : ", titre.text)
-    x = soup.findAll('tr') #Cherche les valeurs du tableau
-    for tr in x:
-        title = tr.find('th')
-        price = tr.find('td')
+parser = argparse.ArgumentParser()
+parser.add_argument("lurl")
+args = parser.parse_args
+print(args.lurl)
 
-        print(title.text, " : ", price.text.replace('Â', ''))
+def scrap_book():
+    book_data = {}
+    url = input("Saisissez l'url du livre à scrapper :   ")  # URL à scraper
+    print("L'url :  ", url)  # Affiche l'Url produit
 
-    image = soup.select("#product_gallery > div > div > div > img")[0]['src'][5:] # Cherche l'Url de l'image
-    print("L'url de l'image :", 'http://books.toscrape.com' + image)
+    request_url = requests.get(url)  # Cherche l'Url en question
+    if request_url.ok:  # Si l'url existe
+        request_url.encoding = 'utf8'
+        soup = BeautifulSoup(request_url.text, 'html.parser')
+        book_data['titre'] = soup.find('h1').text  # Trouve le titre
 
-    cat = soup.select("#default > div > div > ul > li:nth-child(3) > a")[0].text # Cherche la catégorie du livre
-    print("La catégorie est : ", cat)
 
-    description = soup.find(id='product_description').findNext('p').text# Cherche la description
-    print("La description produit : ", description)
+    book_data['universal_product_code']= soup.select_one("#content_inner>article>table>tr:nth-child(1)>td").text
+    book_data['price_excluding_taxe'] = soup.select_one("#content_inner > article > table > tr:nth-child(3) > td").text
+    book_data['prince_including_taxe'] = soup.select_one("#content_inner > article > table > tr:nth-child(4) > td").text
+    book_data['number_available'] = soup.select_one("#content_inner > article > table > tr:nth-child(6) > td").text
+    book_data['image_url'] = soup.select_one("#product_gallery > div > div > div > img")['src'][5:]  # Url de l'image
+    book_data['category'] = soup.select_one("#default > div > div > ul > li:nth-child(3) > a").text
+    book_data['product_description'] = soup.select_one('#content_inner > article > p').text
+    note = soup.select_one('#content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating.Three')['class'][1]
+    book_data['review_rating'] = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five'].index(note)
+    return book_data
 
-    rating = soup.find('p', {"class": "star-rating"})['class'][1] # Cherche la note du livre
-    note = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five']
-    print("Product Rating : ", note.index(rating))
-
-bookOne()
+print(scrap_book())
