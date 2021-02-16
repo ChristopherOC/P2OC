5e9a6da2a0afdfba0944fe04ea747142a11b913a
Rene Christopher <crene.contact@gmail.com>
2021-02-16 23:58:35 +0100

    Programme ok Finir Pep-8

--- scrap_book.py
+++ scrap_book.py
@@ -4,30 +4,41 @@ from bs4 import BeautifulSoup
 import argparse
 
 
-def scrap_book():
-    book_data = {}
+def main():
     parser = argparse.ArgumentParser(description="saisir l'url pour scraper un livre")
     parser.add_argument('url', type=str, help='lien du livre')
     args = parser.parse_args()
+    print(scrap_book(args.url))
 
-    request_url = requests.get(args.url)  # Cherche l'Url en question
+
+
+def scrap_book(url):
+    book_data = {}
+
+    request_url = requests.get(url)  # Cherche l'Url en question
     if request_url.ok:  # Si l'url existe
         request_url.encoding = 'utf8'
         soup = BeautifulSoup(request_url.text, 'html.parser')
         book_data['titre'] = soup.find('h1').text  # Trouve le titre
 
-    book_data['url'] = args.url
-    book_data['universal_product_code'] = soup.select_one("#content_inner>article>table>tr:nth-child(1)>td").text
-    book_data['price_excluding_taxe'] = soup.select_one("#content_inner > article > table > tr:nth-child(3) > td").text
-    book_data['prince_including_taxe'] = soup.select_one("#content_inner > article > table > tr:nth-child(4) > td").text
-    book_data['number_available'] = soup.select_one("#content_inner > article > table > tr:nth-child(6) > td").text
-    book_data['image_url'] = soup.select_one("#product_gallery > div > div > div > img")['src'][5:]  # Url de l'image
-    book_data['category'] = soup.select_one("#default > div > div > ul > li:nth-child(3) > a").text
-    book_data['product_description'] = soup.select_one('#content_inner > article > p').text
-    note = \
-    soup.select_one('#content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating.Three')['class'][1]
-    book_data['review_rating'] = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five'].index(note)
-    return book_data
+        book_data['url'] = url
+        book_data['universal_product_code'] = soup.select_one("#content_inner>article>table>tr:nth-child(1)>td").text
+        book_data['price_excluding_taxe'] = soup.select_one("#content_inner > article > table > tr:nth-child(3) > td").text
+        book_data['prince_including_taxe'] = soup.select_one("#content_inner > article > table > tr:nth-child(4) > td").text
+        book_data['number_available'] = soup.select_one("#content_inner > article > table > tr:nth-child(6) > td").text
+        book_data['image_url'] = soup.select_one(".carousel img")['src'].replace("../../","https://books.toscrape.com/")  # Url de l'image
+        book_data['category'] = soup.select_one("#default > div > div > ul > li:nth-child(3) > a").text
+        description = soup.select_one('#content_inner > article > p')
+        if description is not None:
+            book_data['product_description'] = description.text
+        else:
+            book_data['product_description']= "Aucune description disponible"
+        note = \
+            soup.select_one('#content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating')['class'][1]
+        book_data['review_rating'] = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five'].index(note)
+        print(book_data)
+        return book_data
 
 
-print(scrap_book())
+if __name__ == '__main__':
+    main()
--- scrap_category.py
+++ scrap_category.py
@@ -1,46 +1,74 @@
 import requests
 from bs4 import BeautifulSoup
 import argparse
+import csv
+from scrap_book import scrap_book
+import os.path
 
 
-def scrap_category():
-    parser = argparse.ArgumentParser(description="saisir l'url pour scraper un livre")
-    parser.add_argument('url', type=str, help='lien du livre')
+def scrap_category(url):
+    request_category = requests.get(url)
+
+    if request_category.ok:
+
+        book_data_list = []
+        home_soup = BeautifulSoup(request_category.text, 'html.parser')
+        books_url = [el['href'].replace("../../..", "https://books.toscrape.com/catalogue") for el in
+                     home_soup.select('article h3 a')]
+
+        while True:
+            button = home_soup.select_one("#default > div > div > div > div > section > div:nth-child(2) >\
+                               div > ul > li.next > a")
+
+            if not button:
+                break
+
+            href = url.replace('index.html', button['href'])
+            request_page = requests.get(href)
+            page_soup = BeautifulSoup(request_page.text, 'html.parser')
+            new_list_book = [el['href'].replace("../../..", "https://books.toscrape.com/catalogue") for el in \
+                                 page_soup.select('article h3 a')]
+            for new_book in new_list_book:
+                books_url.append(new_book)
+
+        category = ""
+        for product_page_url in books_url:
+
+            book_data = scrap_book(product_page_url)
+            book_data_list.append(book_data)
+            current_directory = os.getcwd()
+
+            category = book_data['category']
+            category_directory = os.path.join(current_directory, category)
+            if not os.path.exists(category_directory):
+                os.makedirs(category_directory)
+
+            image_directory = os.path.join(category_directory, 'image')
+            if not os.path.exists(image_directory):
+                os.makedirs(image_directory)
+
+            img_data = requests.get(book_data['image_url']).content  # Enregistre les url
+            # des img dans un dossier
+
+            with open(category + '/image/' + book_data['universal_product_code'] + '.jpg', "wb") as dossier_img:
+                dossier_img.write(img_data)
+
+        with open(category + '/' + category + '.csv', 'w', newline='', encoding="utf-8") as csv_file:
+            fieldnames = book_data_list[0].keys()
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+            for book in book_data_list:
+                writer.writerow(book)
+
+        print("Scrap de la catégorie avec succès")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="saisir l'url pour scraper la catégorie")
+    parser.add_argument('url', type=str, help='lien de la catégorie')
     args = parser.parse_args()
-    home = requests.get(args.url)
+    scrap_category(args.url)
 
-    if home.ok:
-        homeSoup = BeautifulSoup(home.text, 'html.parser')
-        books = homeSoup.findAll('article')
-        for book in books:
-            a = book.find('a')
-            link = a['href']
-            each = (link.replace('../../../', 'https://books.toscrape.com/catalogue/'))
-            print(each)
 
-            bookrequest = requests.get(each)  # Cherche l'Url en question
-            if bookrequest.ok: # Si l'url existe
-                bookSoup = BeautifulSoup(bookrequest.text, 'lxml')
-                titre = bookSoup.find('h1')  # Affiche le titre
-                print("Le titre est : ", titre.text)
-
-                x = bookSoup.findAll('tr')
-                for tr in x:
-                    title = tr.find('th')
-                    price = tr.find('td')
-
-                    print(title.text, " : ", price.text.replace('Â', ''))
-
-                image = bookSoup.select("#product_gallery > div > div > div > img")[0]['src'][5:] # Cherche l'url de l'image
-                print("L'url de l'image :", 'http://books.toscrape.com' + image)
-
-                cat = bookSoup.select("#default > div > div > ul > li:nth-child(3) > a")[0].text # Cherche la catégorie
-                print("La catégorie est : ", cat)
-
-                description = bookSoup.find(id='product_description').findNext('p').text # Cherche la description
-                print("La description produit : ", description)
-
-                rating = bookSoup.find('p', {"class": "star-rating"})['class'][1]  # Cherche la note du livre
-                note = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five']
-                print("Product Rating : ", note.index(rating))
-print(scrap_category())
+if __name__ == '__main__':
+    main()
